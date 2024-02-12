8ed39a2175ad7867e0288cf8673224650390165f
JonselLaptop <jonasmazou@gmail.com>
2024-02-12 16:29:50 +0100

    created child process using posix spawn

--- parent.c
+++ parent.c
@@ -3,28 +3,47 @@
 #include <sys/stat.h>
 #include <sys/types.h>
 #include <fcntl.h>
+#include <spawn.h>
 
 //declaring mutex
 pthread_mutex_t mutex;
-const char *path = "./my_pipe";
+const char *pipePath = "./my_pipe";
 
 int main()
 {
     
     //creating pipe
-    int status = mkfifo(path, S_IRUSR | S_IWUSR);
-    if(status == 0){
-        printf("successfully created pipe\npipe-status: %d\n", status);
+    int pipeStatus = mkfifo(pipePath, S_IRUSR | S_IWUSR);
+    if(pipeStatus == 0){
+        printf("successfully created pipe\npipe-status: %d\n", pipeStatus);
     } else {
-        printf("ERROR: could not create pipe\npipe-status: %d\n", status);
+        printf("ERROR: could not create pipe\npipe-status: %d\n", pipeStatus);
     }
 
     //opening the pipe with read and write permissions
-    int fd = open(path, O_RDWR);
+    int fd = open(pipePath, O_RDWR);
+
+    //creating child process
+    pid_t pid;
+
+    //creating argument and environment vectors to start the child process with
+    const char *argv[] = {"./child", NULL};
+    const char *envp[] = {NULL};
+
+    //creating child process
+    int spawnStatus = posix_spawn(&pid, "./child", NULL, NULL, argv, envp); 
+    if(spawnStatus == 0){
+        printf("successfully created child process with pid: %d\n", pid);
+    } else {
+        printf("Failed to create child process. Status: %d\n", spawnStatus);
+    }
+
+    
 
 
     //deleting the file from disk after program is done running
     remove("./my_pipe");
+
 
     //initializing mutex
     pthread_mutex_init(&mutex, NULL);
